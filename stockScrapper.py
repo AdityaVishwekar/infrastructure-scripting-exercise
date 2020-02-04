f8605b187f5198d40aa4aa2837576884bc4eabe4
acvishw@xomdeveloper.com <acvishw@xomdeveloper.com>
2020-02-04 16:55:57 +0000

    added Sort functionality

--- stockScrapper.py
+++ stockScrapper.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 from openpyxl import Workbook
+import operator
+from datetime import date
 
 
 def main():
@@ -15,18 +17,23 @@ def main():
     sheet["D1"] = "Ratio"
 
     # List of companies
-    companies = ["JPM", "MSFT", "GOOGL"]
+    companies = ["JPM", "MSFT", "GOOGL", "BAC", "TSLA", "FB", "V", "MA", "DIS", "QCOM", "AAPL", "SGEN", "NVDA"]
     data = []
     
     for company in companies:
         cash = getTotalCash(company)
         debt = getTotalDebt(company)
-        data.append([company,cash,debt])
+        ratio = calculateRatio(cash, debt)
+        data.append([company,cash,debt,ratio])
+    
+    # Sort the data based on ratio
+    sortedData = sorted(data, key=lambda x: x[3], reverse=True)
         
     # Add data to xlsx
-    for row in data:
+    for row in sortedData:
         sheet.append(row)
-    workbook.save(filename="Reports/test.xlsx")
+    reportName = "Reports/DebtRatioReport"+str(date.today())+".xlsx"
+    workbook.save(filename=reportName)
 
 # Get url
 def setPage(stock):
@@ -46,6 +53,13 @@ def getTotalDebt(stock):
     soup = setPage(stock)
     results = soup.find_all('td', class_='Fz(s) Fw(500) Ta(end) Pstart(10px) Miw(60px)')
     return results[53].text
+
+# Calculate the Ratio
+def calculateRatio(cash, debt):
+    if cash[-1] == debt[-1]:
+        cash = float(cash[0:-1])
+        debt = float(debt[0:-1])
+        return cash/debt
 
 if __name__=='__main__':
     main()
