8a389eeec09f56e08631fee92449c1781a7e0e0b
acvishw@xomdeveloper.com <acvishw@xomdeveloper.com>
2020-02-05 20:59:21 +0000

    refactor

--- cnc-asset-mgt/dbsetup.py
+++ cnc-asset-mgt/dbsetup.py
@@ -18,46 +18,30 @@ def sql_query(c,sql):
     rows = c.fetchall()
     return rows
 
-def main():
-    database = "./pythonsqlite.db"
-    sql_create_report = """ 
-            CREATE TABLE IF NOT EXISTS report (
-                company varchar(225) NOT NULL,
-                total_cash float,
-                total_debt float,
-                ratio float
-            ); 
-        """
-
-    # create a database connection
-    conn = create_connection(database)
-    if conn is not None:
-        # create tables
-        create_table(conn, sql_create_report)
-        print("Connection established!")
-    else:
-        print("Could not establish connection")
-    
-    # load data from excel
-    data_url = '../Reports/DebtRatioReport_IN_2020-02-04.xlsx'
+def load_Data(conn, table, data_url):
     data_table = pd.read_excel(data_url)
-    
-    # data_table.to_sql('report1', conn, dtype={
-    #     'COMPANY' : 'varchar(225)',
-    #     'Total Cash' : 'float',
-    #     'Total Debt' : 'float',
-    #     'Ratio' : 'float',
-    # } )
-
-    data_url = '../Reports/DebtRatioReport_US_2020-02-05.xlsx'
-    data_table = pd.read_excel(data_url)
-    
-    data_table.to_sql('report2', conn, dtype={
+    data_table.to_sql(table, conn, dtype={
         'COMPANY' : 'varchar(225)',
         'Total Cash' : 'float',
         'Total Debt' : 'float',
         'Ratio' : 'float',
     } )
+
+def main():
+    database = "./pythonsqlite.db"
+
+    # create a database connection
+    conn = create_connection(database)
+    if conn is not None:
+        # create tables and load data from excel
+        data_url = '../Reports/DebtRatioReport_IN_2020-02-04.xlsx'
+        load_Data(conn, 'report1', data_url)
+        data_url = '../Reports/DebtRatioReport_US_2020-02-05.xlsx'
+        load_Data(conn, 'report2', data_url)
+        print("Connection established!")
+    else:
+        print("Could not establish connection")
+    
 
 if __name__ == '__main__':
     main()
